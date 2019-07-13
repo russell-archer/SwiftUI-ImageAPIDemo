fd15b85d118c1f320cd282c5df73eb55f20c2353
russell-archer <russell.archer@mac.com>
2019-07-13 20:05:18 +0100

    Initial Commit

---
+++ SwiftUI-ImageAPIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-ImageAPIDemo
+//
+//  Created by Russell Archer on 13/07/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
